c1ad7f83b9dfd18323280466e9922a481ed41746
Mees van Dongen <mvandongen@ilionx.com>
2022-10-24 11:58:53 +0200

    fix: circle animation delay

--- src/components/CirclesRange/CirclesRange.tsx
+++ src/components/CirclesRange/CirclesRange.tsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from "react";
-import * as S from './circle-range.styles';
+import range from "lodash-es/range";
+import React, { useMemo, useState } from "react";
+import * as S from "./circle-range.styles";
 
 interface CirclesRangeProps {
   value: number;
@@ -8,15 +9,10 @@ interface CirclesRangeProps {
   max?: number;
 }
 
-interface Circles {
-  index: number,
-  checked: boolean,
-  delay: number,
-}
-
-enum Direction {
-  FORWARD = 'forward',
-  REVERSED = 'reversed'
+interface Circle {
+  index: number;
+  checked: boolean;
+  delay: number;
 }
 
 export function CirclesRange({
@@ -25,49 +21,28 @@ export function CirclesRange({
   min = 1,
   max = 10,
 }: CirclesRangeProps) {
-  const [circles, setCircles] = useState<Circles[]>([]);
-  let direction: Direction = Direction.FORWARD;
+  const [previousValue, setPreviousValue] = useState(value);
 
-  useEffect(() => {
-    let newCircles: Circles[] = [];
-    let newCheckedCircles: Circles[] = [];
-    let previousCheckedCircles: Circles[] = [];
+  if (previousValue !== value) {
+    setPreviousValue(value);
+  }
 
-    if (circles) {
-      previousCheckedCircles = circles.filter((circle) => {
-        return circle.checked;
-      });
-    }
+  const circles = useMemo(() => {
+    return range(min, max).map((i): Circle => {
+      const delay = Math.abs(previousValue - i) * 50;
 
-    for (let i = min; i <= max; i++) {
-      newCircles.push({
+      return {
         index: i,
         checked: value >= i,
-        delay: 0,
-      });
-    }
-
-    newCheckedCircles = newCircles.filter((circle) => {
-      return circle.checked;
+        delay,
+      };
     });
-
-    direction = newCheckedCircles.length >= previousCheckedCircles.length ? Direction.FORWARD : Direction.REVERSED;
-
-    newCircles.map((circle) => {
-      if (direction === Direction.FORWARD) {
-        circle.delay = circle.index * 50;
-      }
-      if (direction === Direction.REVERSED) {
-        circle.delay = (newCircles.length * 50) - circle.index * 50;
-      }
-    });
-
-    setCircles(newCircles);
-  }, [onChange]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [max, min, value]);
 
   return (
     <div className="flex justify-between">
-      {circles.map(({index, checked, delay}) => (
+      {circles.map(({ index, checked, delay }) => (
         <label key={index} className="flex cursor-pointer">
           <input
             className="invisible peer h-0 w-0"
